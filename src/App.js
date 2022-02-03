9df2a46724f0e816e4f340eeec0d7f31ecd3c53a
Marcela Arroyo <malvesarroyo@gmail.com>
2022-02-03 17:26:15 -0300

    Requisito 1 - Criando as rotas necessárias para a apliacação

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Content from './components/Content';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <main>
+        <p>TrybeTunes</p>
+        <BrowserRouter>
+          <Content />
+        </BrowserRouter>
+      </main>
+    );
   }
 }
 
